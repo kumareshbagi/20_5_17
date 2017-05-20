2820f229a1d89f38c7386f4a551c2ba4c4cd0224
kumareshbagi <kumaresh.517@gmail.com>
2017-05-20 18:42:32 +0530

    Add files via upload

---
+++ ci/apllications/views/swap.php
@@ -0,0 +1,16 @@
+<?php
+
+$a=10;
+$b=20;
+
+echo "Value of a: $a</br>";
+echo "Value of b: $b</br>";
+
+$temp=$a;
+$a=$b;
+$b=$temp;
+
+echo "Value of a after swaping: $a</br>";
+echo "Value of b after swaping: $b</br>";
+
+?>
---
+++ ci/apllications/views/t1.php
@@ -0,0 +1,15 @@
+<?php
+  
+$a=10;
+$b=20;
+$c=$a+$b;
+
+echo "Sum: ",$c;
+echo "<br>";
+$x=$a*$b;
+echo "Multiplication: ",$x;
+echo "<br>";
+$y=$b/$a;
+echo "Division: ",$y;
+
+?>
